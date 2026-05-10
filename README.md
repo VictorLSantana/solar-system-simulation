4c83e50c437b8003b696068b90356a7935aa2c00
Victor Lucas Santana <costalucas999@gmail.com>
2026-05-10 13:33:03 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# Links
+- https://thepythoncodingbook.com/2021/09/29/simulating-orbiting-planets-in-a-solar-system-using-python-orbiting-planets-series-1/
+- https://www.youtube.com/playlist?list=PLMi6KgK4_mk2-hB3RJ4Vf_OY9O-ejpdh9
---
+++ main.py
@@ -0,0 +1,28 @@
+
+import turtle
+
+
+class SolarSystemBody(turtle.Turtle):
+    pass
+
+class Sun(SolarSystemBody):
+    pass
+
+class Planet(SolarSystemBody):
+    pass
+
+
+class SolarSystem:
+    def __init__(self, width, height):
+        self.solar_system = turtle.Screen()
+        self.solar_system.tracer(0)
+        self.solar_system.setup(width, height)
+        self.solar_system.bgcolor("black")
+        
+        self.bodies = []
+        
+    def add_body(self, body):
+        self.bodies.append(body)
+        
+    def remove_body(self, body):
+        self.bodies.remove(body) 
