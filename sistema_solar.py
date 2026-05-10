0a77517576d4d69510b4c7d9f6cd0ca211cb430e
Victor Lucas Santana <costalucas999@gmail.com>
2026-05-10 19:15:19 -0300

    Implement position update and movement methods for celestial bodies; adjust simulation dimensions

--- sistema_solar.py
+++ sistema_solar.py
@@ -1,5 +1,6 @@
 
 import turtle
+import itertools
 
 class SistemaSolar:
     def __init__(self, largura, altura):
@@ -16,6 +17,12 @@ class SistemaSolar:
         
     def remove_corpo(self, corpo):
         self.corpos.remove(corpo) 
+        
+    def atualiza_posicao(self):
+        for corpo in self.corpos:
+            corpo.movimento()
+            corpo.desenha_na_tela()
+            self.sistema_solar.update()
 
 class CorpoCeleste(turtle.Turtle):
     
@@ -31,10 +38,17 @@ class CorpoCeleste(turtle.Turtle):
         
         sistema_solar.adicionar_corpo(self) # adicionando o corpo ao sistema solar
         
-    def draw(self):
+    def desenha_na_tela(self):
+        self.clear()
         self.dot(self.tamanho)
         
 
+    def movimento(self):
+        self.setx(self.xcor() + self.velocidade[0])
+        self.sety(self.ycor() + self.velocidade[1])
+    
+    
+    
 class Sol(CorpoCeleste):
     def __init__(self, sistema_solar, massa, tamanho, posicao=(0, 0), velocidade=(0, 0)):
         super().__init__(sistema_solar, massa, tamanho, posicao, velocidade)
--- sistema_solar_simples.py
+++ sistema_solar_simples.py
@@ -6,14 +6,13 @@ from sistema_solar import SistemaSolar, Sol
 
 MASSA_SOL = 10_000_000
 TAMANHO_SOL = 100
-LARGURA_TELA = 800
-ALTURA_TELA = 550
+LARGURA_TELA = 1000
+ALTURA_TELA = 700
 
 
 sistema_solar = SistemaSolar(largura=LARGURA_TELA, altura=ALTURA_TELA)
 
-sol = Sol(sistema_solar=sistema_solar, massa=MASSA_SOL, tamanho=TAMANHO_SOL)
+sol = Sol(sistema_solar=sistema_solar, massa=MASSA_SOL, tamanho=TAMANHO_SOL, velocidade=(0.1,0.1))
 
-sol.draw()
-turtle.done()
-
+while True:
+    sistema_solar.atualiza_posicao()
