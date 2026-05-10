a72b700372e2f3a61a4b7c74edb449fc2579bcaf
Victor Lucas Santana <costalucas999@gmail.com>
2026-05-10 16:44:01 -0300

    Refactor SolarSystem and SolarSystemBody classes for improved structure and clarity

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 
 import turtle
 
-
-class SolarSystemBody(turtle.Turtle):
-    pass
-
-class Sun(SolarSystemBody):
-    pass
-
-class Planet(SolarSystemBody):
-    pass
-
-
 class SolarSystem:
     def __init__(self, width, height):
-        self.solar_system = turtle.Screen()
-        self.solar_system.tracer(0)
-        self.solar_system.setup(width, height)
-        self.solar_system.bgcolor("black")
+        self.solar_system = turtle.Screen() 
+        self.solar_system.tracer(0) # set delay of update drawing
+        self.solar_system.setup(width, height) #size of the screen
+        self.solar_system.bgcolor("black") # background color
         
         self.bodies = []
         
@@ -26,3 +15,25 @@ class SolarSystem:
         
     def remove_body(self, body):
         self.bodies.remove(body) 
+
+class SolarSystemBody(turtle.Turtle):
+    def __init__(self, solar_system, mass, position=(0, 0), velocity=(0, 0)):
+        super().__init__()
+        self.mass = mass,
+        self.setposition(position)
+        self.velocity = velocity
+        
+        self.penup()
+        self.hideturtle()
+        
+        solar_system.add_body(self) #solar system that the body (self) belongs to
+        
+        
+    pass
+
+class Sun(SolarSystemBody):
+    pass
+
+class Planet(SolarSystemBody):
+    pass
+
